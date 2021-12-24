f4e61c4279ac5ce946c1cc83432dd70f77ffa19c
mubashir-cvr <mubashircheruvayoor@gmail.com>
2021-12-24 17:15:46 +0530

    Form

---
+++ stocks/froms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from django.forms.widgets import SelectMultiple, TextInput, Textarea, EmailInput, CheckboxInput,URLInput, Select, NumberInput, RadioSelect, FileInput
+from apis.models import *
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        exclude = ('timestamp',)
+        widgets = {
+            'name': TextInput(attrs={'class': 'required form-control', 'placeholder': 'Name'}),
+            'title': TextInput(attrs={'class': 'required form-control', 'placeholder': 'Title'}),
+            'description': TextInput(attrs={'class': 'required form-control', 'placeholder': 'Description'}),
+        }
+        
--- stocks/views.py
+++ stocks/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from apis.models import *
+from .froms import *
+from django.http import HttpResponse
+import json
 # Create your views here.
 
 
@@ -20,7 +24,27 @@ def editproduct(request):
     return render(request,'stocks/editproduct.html')
 
 def listcategories(request):
-    return render(request,'stocks/listcategories.html')
+    form = CategoryForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            response_data = {
+                "status" : "true",
+                "title" : "Successfully Submitted",
+                "message" : "Message successfully updated"
+            }
+        else:
+            print (form.errors)
+            response_data = {
+                "status" : "false",
+                "title" : "Form validation error",
+            }
+            return HttpResponse(json.dumps(response_data), content_type='application/javascript')
+    context={
+        'cateregories':Category.objects.all(),
+        'form':form
+        }
+    return render(request,'stocks/listcategories.html',context)
 
 def addcategory(request):
     return render(request,'stocks/addcategory.html')
