3a5ec7749cf1f698e785707eadfd7ae0e3b053da
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-05 17:20:21 +0530

    Bug fix

--- apis/serializerhelper.py
+++ apis/serializerhelper.py
@@ -5,6 +5,6 @@ def CalculateOfferPercentage(product):
     offer=Offer.objects.filter(product=product)
     if offer.exists():
         offPrice=product.price-offer.first().offerPrice
-        percentage=(product.price-offPrice)*100/product.price
+        percentage=(offPrice)*100/product.price
         return str(int(percentage))+'%'
     return 0
